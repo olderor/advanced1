522e3bc4544a10ceeb679e9bc1dda869f5ecf575
olderor <98body@gmail.com>
2016-11-03 00:35:39 +0200

    code optimization

--- nsuts++/main.cpp
+++ nsuts++/main.cpp
@@ -1,66 +1,107 @@
 //  Created by olderor on 22.10.16.
 //  Copyright © 2016 olderor. All rights reserved.
 
-#include <fstream>
+#include <iostream>
 #include <algorithm>
 #include <queue>
+#include <vector>
 
-int peopleWeights[301];
-int weightsPerOneBus[301][301];
-int optimalWeights[301][101];
+int busCount, peopleCount, busCapacity;
 
-int main() {
+std::vector<int>              peopleWeights;
+std::vector<std::vector<int>> weightsPerOneBus;
+std::vector<std::vector<int>> optimalWeights;
 
-    std::ifstream cin("c.in");
-    std::ofstream cout("c.out");
 
-    int busCount, peopleCount, busCapacity;
-    cin >> busCount >> busCapacity >> peopleCount;
+void clearData() {
+    peopleWeights.clear();
+    busCount = 0;
+    peopleCount = 0;
+    busCapacity = 0;
+}
+
+void readData(std::istream& _Istr) {
+
+    clearData();
+
+    _Istr >> busCount >> busCapacity >> peopleCount;
+
+    peopleWeights.resize(peopleCount);
 
     for (int i = 0; i < peopleCount; ++i) {
-        cin >> peopleWeights[i];
+        _Istr >> peopleWeights[i];
     }
+}
 
-    for (int counterI = peopleCount - 1; counterI >= 0; --counterI) {
+void setWeightsPerOneBus() {
+
+    weightsPerOneBus.clear();
+    weightsPerOneBus.resize(peopleCount, std::vector<int>(peopleCount));
+
+    for (int peopleStartIndex = peopleCount - 1; peopleStartIndex >= 0; --peopleStartIndex) {
+
         std::priority_queue<int> currentWeights;
         int currentWeightsSum = 0;
         int currentWeightsSize = 0;
 
-        for (int counterJ = counterI; counterJ < peopleCount; ++counterJ) {
+        for (int peopleEndIndex = peopleStartIndex; peopleEndIndex < peopleCount; ++peopleEndIndex) {
 
-            if (peopleWeights[counterJ] <= busCapacity - currentWeightsSum) {
-                currentWeights.push(peopleWeights[counterJ]);
-                currentWeightsSum += peopleWeights[counterJ];
+            if (peopleWeights[peopleEndIndex] <= busCapacity - currentWeightsSum) {
+                currentWeights.push(peopleWeights[peopleEndIndex]);
+                currentWeightsSum += peopleWeights[peopleEndIndex];
                 ++currentWeightsSize;
             }
             else {
-                int top = currentWeights.top();
-                if (top > peopleWeights[counterJ]) {
+                const int top = currentWeights.top();
+                if (top > peopleWeights[peopleEndIndex]) {
                     currentWeights.pop();
-                    currentWeights.push(peopleWeights[counterJ]);
-                    currentWeightsSum += peopleWeights[counterJ] - top;
+                    currentWeights.push(peopleWeights[peopleEndIndex]);
+                    currentWeightsSum += peopleWeights[peopleEndIndex] - top;
                 }
             }
 
-            weightsPerOneBus[counterI][counterJ] = currentWeightsSize;
+            weightsPerOneBus[peopleStartIndex][peopleEndIndex] = currentWeightsSize;
         }
     }
+}
+
+void setOptimalWeights() {
+
+    optimalWeights.clear();
+    optimalWeights.resize(peopleCount, std::vector<int>(busCount, 0));
 
     for (int j = 0; j < busCount; ++j) {
         for (int i = peopleCount - 1; i >= 0; --i) {
 
             optimalWeights[i][j] = std::max(optimalWeights[i][j], weightsPerOneBus[0][i]);
+
+            if (j == 0) {
+                continue;
+            }
+
             for (int k = 0; k < i; ++k) {
                 optimalWeights[i][j] = std::max(optimalWeights[i][j],
                     optimalWeights[k][j - 1] + weightsPerOneBus[k + 1][i]);
             }
         }
     }
+}
 
-    cout << optimalWeights[peopleCount - 1][busCount - 1];
+int solveProblem() {
+    setWeightsPerOneBus();
+    setOptimalWeights();
+    return optimalWeights[peopleCount - 1][busCount - 1];
+}
 
-    cin.close();
-    cout.close();
+void writeData(std::ostream& _Istr, int data) {
+    _Istr << data << std::endl;
+}
+
+int main() {
+
+    readData(std::cin);
+    const int answer = solveProblem();
+    writeData(std::cout, answer);
 
     return 0;
 }
